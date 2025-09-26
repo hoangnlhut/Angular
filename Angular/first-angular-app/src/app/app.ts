57fcbd455d248f3595cbfec08cdde28f6ecd8878
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-09-26 09:25:24 +0700

    add new header component to our app root component and add new component to root html file

--- Angular/first-angular-app/src/app/app.ts
+++ Angular/first-angular-app/src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('first-angular-app');
+  protected readonly title = signal('first-angular-app made by Hoang');
 }
