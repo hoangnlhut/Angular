15ff0eddc77ce3c67854403a071735d6581f1465
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-09-29 10:01:33 +0700

    USING @Input Decorator Feature: to get data passed from app component to user-component and then display it to user html

--- Angular/first-angular-app/src/app/user/user.ts
+++ Angular/first-angular-app/src/app/user/user.ts
@@ -1,5 +1,5 @@
-import { Component, computed, effect, linkedSignal, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, computed, effect, Input, linkedSignal, signal } from '@angular/core';
+
 
 @Component({
   selector: 'app-user',
@@ -8,25 +8,15 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.css'
 })
 export class User {
+  @Input() avartar!: string;
+  @Input() name!: string;
+  @Input() id!: string;
 
-  users = signal(DUMMY_USERS);
-
-  // computedUsers = computed(() => this.users());
-  // effectUsers = effect(() => this.users());
-  // userCustomTheme = linkedSignal(() => this.users());
-  
-  getCorrectImagePath(avatar: string) {
-    return 'assets/users/' + avatar;
+  get imagePath() {
+    return `assets/users/${this.avartar}`;
   }
 
-
-  // CAN't NOT USE THIS BECAUSE WE ARE NOW USING SIGNAL
-  // get imagePath() {
-  //   return this.imageBasePath;
-  // }
-
   onSelectUser(user: { id: string; name: string; avatar: string; }) {
-    alert('User selected!');
     console.log(user);
   } 
 } 
