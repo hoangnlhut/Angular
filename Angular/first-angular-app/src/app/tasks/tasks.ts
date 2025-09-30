dc33a0c534c2285da1c3e6df609796958b653638
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-09-30 17:08:15 +0700

    Tasks Component: update style css, html and code in ts file

--- Angular/first-angular-app/src/app/tasks/tasks.ts
+++ Angular/first-angular-app/src/app/tasks/tasks.ts
@@ -1,14 +1,31 @@
-import { Component, computed, Input, input, signal } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
+import { Task } from './task/task';
+import { DUMMY_USERS } from '../dummy-users';
+import { IUser } from '../user/user.model';
+
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [Task],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css'
 })
 export class Tasks {
-  @Input() nameUser :string | undefined ; // this union type --> 
+  userData = signal(DUMMY_USERS);
+
+  @Input({required: true}) user!: IUser ; // this union type --> 
   //  @Input() nameUser?:string   --> aqual to above 
 
-;
+  AddTask(idUser: string) {
+
+    const singleUserData =  this.userData().find(u => u.id === idUser);
+    if (singleUserData) {
+      const newTask = { id: 'newTaskId', title: 'New Task', completed: false, dueDate: new Date(), summary: 'This is a summary of the newTaskId task.'  };
+      singleUserData.tasks.push(newTask);
+    }
+    else 
+    {
+      throw new Error('Method not implemented.');
+    }
+  }
 }
