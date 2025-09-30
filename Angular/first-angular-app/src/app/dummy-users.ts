1488e78056952c8894bce41beea96054c171d32c
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-09-30 17:06:56 +0700

    update data dummy file

--- Angular/first-angular-app/src/app/dummy-users.ts
+++ Angular/first-angular-app/src/app/dummy-users.ts
@@ -3,30 +3,60 @@ export const DUMMY_USERS = [
     id: 'u1',
     name: 'Jasmine Washington',
     avatar: 'user-1.jpg',
+    tasks: [
+      { id: 'u1t1', title: 'Finish Angular project', completed: false, dueDate: new Date(), summary: 'This is a summary of the task.' },
+      { id: 'u1t2', title: 'Review pull requests', completed: true, dueDate: new Date(), summary: 'This is a summary of the task.'  },
+      { id: 'u1t3', title: 'Update documentation', completed: false, dueDate: new Date(), summary: 'This is a summary of the task.' },
+    ]
   },
   {
     id: 'u2',
     name: 'Emily Thompson',
     avatar: 'user-2.jpg',
+    tasks: [
+      { id: 'u2t1', title: 'Finish Angular project Emily Thompson', completed: false, dueDate: new Date(), summary: 'This is a summary of the task.'   },
+      { id: 'u2t2', title: 'Review pull requests Emily Thompson', completed: true, dueDate: new Date(), summary: 'This is a summary of the task.'  },
+      { id: 'u2t3', title: 'Update documentation Emily Thompson', completed: false, dueDate: new Date(), summary: 'This is a summary of the task.'  },
+    ]
   },
   {
     id: 'u3',
     name: 'Marcus Johnson',
     avatar: 'user-3.jpg',
+    tasks: [
+      { id: 'u3t1', title: 'Finish Angular project Marcus Johnson', completed: false, dueDate: new Date(), summary: 'This is a summary of the task.'  },
+      { id: 'u3t2', title: 'Review pull requests Marcus Johnson', completed: true, dueDate: new Date(), summary: 'This is a summary of the task.'  },
+      { id: 'u3t3', title: 'Update documentation Marcus Johnson', completed: false, dueDate: new Date(), summary: 'This is a summary of the task.'  },
+    ]
   },
   {
     id: 'u4',
     name: 'David Miller',
     avatar: 'user-4.jpg',
+    tasks: [
+      { id: 'u4t1', title: 'Finish Angular project David Miller', completed: false, dueDate: new Date(), summary: 'This is a summary of the task.'  },
+      { id: 'u4t2', title: 'Review pull requests David Miller', completed: true, dueDate: new Date(), summary: 'This is a summary of the task.'  },
+      { id: 'u4t3', title: 'Update documentation David Miller', completed: false, dueDate: new Date(), summary: 'This is a summary of the task.'  },
+    ]
   },
   {
     id: 'u5',
     name: 'Priya Patel',
     avatar: 'user-5.jpg',
+    tasks: [
+      { id: 'u5t1', title: 'Finish Angular project Priya Patel', completed: false, dueDate: new Date(), summary: 'This is a summary of the task.'  },
+      { id: 'u5t2', title: 'Review pull requests Priya Patel', completed: true, dueDate: new Date(), summary: 'This is a summary of the task.'  },
+      { id: 'u5t3', title: 'Update documentation Priya Patel', completed: false, dueDate: new Date(), summary: 'This is a summary of the task.'  },
+    ]
   },
   {
     id: 'u6',
     name: 'Arjun Singh',
     avatar: 'user-6.jpg',
+    tasks: [
+      { id: 'u6t1', title: 'Finish Angular project Arjun Singh', completed: false, dueDate: new Date(), summary: 'This is a summary of the task.'  },
+      { id: 'u6t2', title: 'Review pull requests Arjun Singh', completed: true, dueDate: new Date(), summary: 'This is a summary of the task.'  },
+      { id: 'u6t3', title: 'Update documentation Arjun Singh', completed: false, dueDate: new Date(), summary: 'This is a summary of the task.'  },
+    ]
   },
 ];
