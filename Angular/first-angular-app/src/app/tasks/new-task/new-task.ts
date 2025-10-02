5396d65bcdc0e64184c4ccebf5f382defa29dcf8
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-02 11:17:02 +0700

    New-task components : using data to save new task

--- Angular/first-angular-app/src/app/tasks/new-task/new-task.ts
+++ Angular/first-angular-app/src/app/tasks/new-task/new-task.ts
@@ -1,5 +1,12 @@
-import { Component, Input, model, output } from '@angular/core';
+import { Component, Input, model, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { DUMMY_USERS } from '../../dummy-users';
+
+
+function generateRandomId(): string {
+  return Math.random().toString(36).substring(2, 9); // Generates a 7-character string
+}
+
 @Component({
   selector: 'app-new-task',
   imports: [FormsModule],
@@ -13,20 +20,38 @@ export class NewTask {
   enteredTitle = '';
   enteredSummary = '';
   enteredDueDate = '';
+
+   userData = signal(DUMMY_USERS);
    
   onCancel(){
         this.cancel.emit();
   }
 
   onSubmit(userId: string) {
-    console.log('START LOGGING');
-    console.log('User ID in NewTask Component: ' + userId);
-    console.log('Title: ' + this.enteredTitle);
-    console.log('Summary: ' + this.enteredSummary);
-    console.log('Due Date: ' + this.enteredDueDate);
-    console.log('END LOGGING');
+    // console.log('START LOGGING');
+    // console.log('User ID in NewTask Component: ' + userId);
+    // console.log('Title: ' + this.enteredTitle);
+    // console.log('Summary: ' + this.enteredSummary);
+    // console.log('Due Date: ' + this.enteredDueDate);
+    // console.log('END LOGGING');
 
     // You can add further logic here to handle the new task creation
+    const singleUserData =  this.userData().find(u => u.id === userId);
+    if (singleUserData) {
+      const newTask = { 
+        id: generateRandomId(), 
+        title: this.enteredTitle,
+        completed: false, 
+        dueDate: new Date(this.enteredDueDate), 
+        summary: this.enteredSummary  
+      };
+      singleUserData.tasks.unshift(newTask);
+    }
+    else 
+    {
+      throw new Error('Method not implemented.');
+    }
+
     this.cancel.emit();
   }
 }
--- Angular/first-angular-app/src/app/tasks/tasks.ts
+++ Angular/first-angular-app/src/app/tasks/tasks.ts
@@ -4,9 +4,7 @@ import { DUMMY_USERS } from '../dummy-users';
 import { IUser } from '../user/user.model';
 import { NewTask } from './new-task/new-task';
 
-function generateRandomId(): string {
-  return Math.random().toString(36).substring(2, 9); // Generates a 7-character string
-}
+
 
 @Component({
   selector: 'app-tasks',
@@ -15,26 +13,12 @@ function generateRandomId(): string {
   styleUrl: './tasks.css'
 })
 export class Tasks {
-
-  userData = signal(DUMMY_USERS);
-
   @Input({required: true}) user!: IUser ;
   // this union type --> 
   isAddingTask = false;
   //  @Input() nameUser?:string   --> aqual to above 
 
   AddTask(idUser: string) {
-
-    // const singleUserData =  this.userData().find(u => u.id === idUser);
-    // if (singleUserData) {
-    //   const newTask = { id: generateRandomId(), title: 'New Task', completed: false, dueDate: new Date(), summary: 'This is a summary of the newTaskId task.'  };
-    //   singleUserData.tasks.push(newTask);
-    // }
-    // else 
-    // {
-    //   throw new Error('Method not implemented.');
-    // }
-
     this.isAddingTask = true;
   }
 
