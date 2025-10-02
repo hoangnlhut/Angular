28ab47c4bafecbc4279c3e4682ceb9ee55c59c3f
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-02 10:43:31 +0700

    update task component

--- Angular/first-angular-app/src/app/tasks/task/task.ts
+++ Angular/first-angular-app/src/app/tasks/task/task.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, output, signal } from '@angular/core';
 import { ITask } from './task.model';
+import { DUMMY_USERS } from '../../dummy-users';
 
 @Component({
   selector: 'app-task',
@@ -9,4 +10,19 @@ import { ITask } from './task.model';
 })
 export class Task {
   @Input() task?: ITask ;
+  userData = signal(DUMMY_USERS);
+
+  displayNewTask = output<boolean>();
+
+  completeTask(idTask?: string) {
+    for (const user of this.userData()) {
+      const task = user.tasks.find(t => t.id === idTask);
+      if (task) {
+        task.completed = !task.completed;
+        break; // Exit the loop once the task is found and updated
+      }
+    }
+
+    this.displayNewTask.emit(true);
+  }
 }
