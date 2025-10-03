954e09eade132b9716a6215a066b9c9dd3677309
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-03 16:50:04 +0700

    Dummy Task data

---
+++ Angular/first-angular-app/src/app/dummy-tasks.ts
@@ -0,0 +1,21 @@
+export const DUMMY_TASKS = [
+    { id: 'u1t1', userId: 'u1',  title: 'Finish Angular project', completed: false, dueDate: new Date().getDate().toString(), summary: 'This is a summary of the task.'},
+    { id: 'u1t2',  userId: 'u1', title: 'Review pull requests', completed: true, dueDate: new Date().getDate().toString(), summary: 'This is a summary of the task.'},
+    { id: 'u1t3',  userId: 'u1', title: 'Update documentation', completed: false, dueDate: new Date().getDate().toString(), summary: 'This is a summary of the task.'},
+    { id: 'u2t1', userId: 'u2', title: 'Finish Angular project Emily Thompson', completed: false, dueDate: new Date().getDate().toString(), summary: 'This is a summary of the task.'   },
+    { id: 'u2t2', userId: 'u2', title: 'Review pull requests Emily Thompson', completed: true, dueDate: new Date().getDate().toString(), summary: 'This is a summary of the task.'  },
+    { id: 'u2t3', userId: 'u2', title: 'Update documentation Emily Thompson', completed: false, dueDate: new Date().getDate().toString(), summary: 'This is a summary of the task.'  },
+    { id: 'u3t1', userId: 'u3', title: 'Finish Angular project Marcus Johnson', completed: false, dueDate: new Date().getDate().toString(), summary: 'This is a summary of the task.'  },
+      { id: 'u3t2',  userId: 'u3', title: 'Review pull requests Marcus Johnson', completed: true, dueDate: new Date().getDate().toString(), summary: 'This is a summary of the task.'  },
+      { id: 'u3t3', userId: 'u3', title: 'Update documentation Marcus Johnson', completed: false, dueDate: new Date().getDate().toString(), summary: 'This is a summary of the task.'  },
+      { id: 'u4t1', userId: 'u4',title: 'Finish Angular project David Miller', completed: false, dueDate: new Date().getDate().toString(), summary: 'This is a summary of the task.'  },
+      { id: 'u4t2', userId: 'u4',title: 'Review pull requests David Miller', completed: true, dueDate: new Date().getDate().toString(), summary: 'This is a summary of the task.'  },
+      { id: 'u4t3', userId: 'u4',title: 'Update documentation David Miller', completed: false, dueDate: new Date().getDate().toString(), summary: 'This is a summary of the task.'  },
+      { id: 'u5t1', userId: 'u5', title: 'Finish Angular project Priya Patel', completed: false, dueDate: new Date().getDate().toString(), summary: 'This is a summary of the task.'  },
+      { id: 'u5t2', userId: 'u5', title: 'Review pull requests Priya Patel', completed: true, dueDate: new Date().getDate().toString(), summary: 'This is a summary of the task.'  },
+      { id: 'u5t3', userId: 'u5', title: 'Update documentation Priya Patel', completed: false, dueDate: new Date().getDate().toString(), summary: 'This is a summary of the task.'  },
+      { id: 'u6t1', userId: 'u6', title: 'Finish Angular project Arjun Singh', completed: false, dueDate: new Date().getDate().toString(), summary: 'This is a summary of the task.'  },
+      { id: 'u6t2', userId: 'u6',title: 'Review pull requests Arjun Singh', completed: true, dueDate: new Date().getDate().toString(), summary: 'This is a summary of the task.'  },
+      { id: 'u6t3', userId: 'u6',title: 'Update documentation Arjun Singh', completed: false, dueDate: new Date().getDate().toString(), summary: 'This is a summary of the task.'  }
+ 
+];
