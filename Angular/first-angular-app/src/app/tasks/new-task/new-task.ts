8681dd0d66cf51b4906ef8a94a6ff512026a51a7
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-04 22:11:05 +0700

    Component New Task: refactor code

--- Angular/first-angular-app/src/app/tasks/new-task/new-task.ts
+++ Angular/first-angular-app/src/app/tasks/new-task/new-task.ts
@@ -1,6 +1,7 @@
-import { Component, Input, model, output, signal } from '@angular/core';
+import { Component, Input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { DUMMY_USERS } from '../../dummy-users';
+import { TasksService } from '../tasks.service';
+import { ITaskModel } from '../task/task.model';
 
 
 function generateRandomId(): string {
@@ -14,44 +15,23 @@ function generateRandomId(): string {
   styleUrl: './new-task.css'
 })
 export class NewTask {
-  @Input({required: true}) userId!: string ;
+
   cancel = output<void>();
+  addTask = output<ITaskModel>();
 
-  enteredTitle = '';
-  enteredSummary = '';
-  enteredDueDate = '';
+  enteredTitleModel= '';
+  enteredSummaryModel = '';
+  enteredDueDateModel = '';
 
-   userData = signal(DUMMY_USERS);
-   
   onCancel(){
         this.cancel.emit();
   }
 
-  onSubmit(userId: string) {
-    // console.log('START LOGGING');
-    // console.log('User ID in NewTask Component: ' + userId);
-    // console.log('Title: ' + this.enteredTitle);
-    // console.log('Summary: ' + this.enteredSummary);
-    // console.log('Due Date: ' + this.enteredDueDate);
-    // console.log('END LOGGING');
-
-    // You can add further logic here to handle the new task creation
-    const singleUserData =  this.userData().find(u => u.id === userId);
-    if (singleUserData) {
-      const newTask = { 
-        id: generateRandomId(), 
-        title: this.enteredTitle,
-        completed: false, 
-        dueDate: new Date(this.enteredDueDate), 
-        summary: this.enteredSummary  
-      };
-      singleUserData.tasks.unshift(newTask);
-    }
-    else 
-    {
-      throw new Error('Method not implemented.');
-    }
-
-    this.cancel.emit();
+  onSubmit() {
+    this.addTask.emit({ 
+      title: this.enteredTitleModel,
+      dueDate: this.enteredDueDateModel,
+      summary: this.enteredSummaryModel  
+    });
   }
 }
