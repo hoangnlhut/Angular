b0d5e39be972c7999013859035966859b7c0dfcb
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-06 11:31:18 +0700

    Modules In Angular: set up App Components as the root component when our app start

--- Angular/first-angular-app/src/app/app.module.ts
+++ Angular/first-angular-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { App } from "./app";
 
 @NgModule({
     declarations: [App],  //declare and register components, directives, pipes that need to work togerther
+    bootstrap: [App],  //specify the root component that Angular should bootstrap when it starts the application
     imports: [],
     exports: []
 })
