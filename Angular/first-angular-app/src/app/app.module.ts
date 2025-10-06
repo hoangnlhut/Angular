55686bac0a3ef7d8dec3f65c09bf59e9680cbb2c
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-06 14:40:11 +0700

    Modules In Angular: Declaring and using components: Note that : declaration only for non-standalone component but imports can be any standalone components and modules

--- Angular/first-angular-app/src/app/app.module.ts
+++ Angular/first-angular-app/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from "@angular/core";
 import { App } from "./app";
+import { Header } from "./header/header";
+import { User } from "./user/user";
+import { Tasks } from "./tasks/tasks";
+import { BrowserModule } from "@angular/platform-browser";
 
 @NgModule({
-    declarations: [App],  //declare and register components, directives, pipes that need to work togerther
+    declarations: [App],  //declare and register only non-standalone components
     bootstrap: [App],  //specify the root component that Angular should bootstrap when it starts the application
-    imports: [],
-    exports: []
+    imports: [BrowserModule, Header, Tasks, User] //this array can be standalone components, modules or for including other modules
 })
 export class AppModule {
 
