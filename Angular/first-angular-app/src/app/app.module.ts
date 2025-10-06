a463a4bb1894754b386a183496b73a46330770a9
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-06 11:21:26 +0700

    Modules In Angular: create new app module file

---
+++ Angular/first-angular-app/src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { App } from "./app";
+
+@NgModule({
+    declarations: [App],  //declare and register components, directives, pipes that need to work togerther
+    imports: [],
+    exports: []
+})
+export class AppModule {
+
+}
