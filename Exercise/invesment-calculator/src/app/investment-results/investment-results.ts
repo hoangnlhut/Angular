d2836179d6a9f60ba03dd73c4ed72ab656d2291b
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-08 18:56:56 +0700

    Formatting input with a pipe using CurrencyPipe of angular

--- Exercise/invesment-calculator/src/app/investment-results/investment-results.ts
+++ Exercise/invesment-calculator/src/app/investment-results/investment-results.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import type { InvestmentResult } from './investment-input.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-investment-results',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.html',
   styleUrl: './investment-results.css'
 })
