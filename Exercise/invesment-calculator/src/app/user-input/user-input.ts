680d84d8f940351d0c41a7e9ddb608ef1e3c63e2
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-08 14:53:00 +0700

    Header component: add style, handling form submission

--- Exercise/invesment-calculator/src/app/user-input/user-input.ts
+++ Exercise/invesment-calculator/src/app/user-input/user-input.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.html',
   styleUrl: './user-input.css'
 })
 export class UserInput {
+
+  enteredInitialInvestment: any;
+  enteredAnnualInvestment: any;
+  enteredExpectedReturn: any;
+  enteredDuration: any;
+
+  onSubmit(){
+    throw new Error('Method not implemented.');
+  }
 
 }
