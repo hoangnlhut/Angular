716f7fd374f104b7dd38c0ed4faf7d0ced87e47e
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-10 09:43:51 +0700

    restructure folder of project and update template of app-root

--- Angular/deep-dive-1-component-template/src/app/dashboard/server-status/server-status.component.ts
+++ Angular/deep-dive-1-component-template/src/app/dashboard/server-status/server-status.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { DashboardItemComponent } from '../dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-server-status',
   standalone: true,
-  imports: [],
+  imports: [DashboardItemComponent],
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
 export class ServerStatusComponent {
-
+  currentStatus = 'online';
 }
--- Angular/deep-dive-1-component-template/src/app/dashboard/tickets/tickets.component.ts
+++ Angular/deep-dive-1-component-template/src/app/dashboard/tickets/tickets.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { DashboardItemComponent } from '../dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [],
+  imports: [DashboardItemComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css'
 })
