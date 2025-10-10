364287e0a8a699e39191a1e2672d8212174d8894
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-10 10:42:40 +0700

    Component Input Repitition

--- Angular/deep-dive-1-component-template/src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ Angular/deep-dive-1-component-template/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard-item.component.css'
 })
 export class DashboardItemComponent {
+  // @Input({required: true}) imgInfo !: {src: string, alt: string};
+  // @Input({required: true}) h2Info !: string; 
 
+
+  //2nd approach
+  imgInfo = input.required<{src: string, alt: string}>();
+  h2Info = input.required<string>();
 }
