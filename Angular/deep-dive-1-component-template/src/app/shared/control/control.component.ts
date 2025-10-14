d705c30dcc47ad9f04e7af140320e1d113a23f48
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-14 10:48:28 +0700

    116. Interacting with Host Elements via @HostListener & @HostBinding

--- Angular/deep-dive-1-component-template/src/app/shared/control/control.component.ts
+++ Angular/deep-dive-1-component-template/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, input} from '@angular/core';
+import { Component, HostBinding, HostListener, ViewEncapsulation, input} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -7,11 +7,25 @@ import { Component, ViewEncapsulation, input} from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
-  host: {   
-    class: 'control',
-    id: 'hoang'
+  //first way to add class in this host element
+  // by using host properties (RECOMMENDED PREFERD ON DECORATION OBJECT)
+  host: {    
+    class: 'control', // for binding properties
+    '(click)' : 'onClick()', // for binding event
   }
 })
 export class ControlComponent {
-    label = input<string>();
+  // 2nd way to add class (NOT recommended)
+  // Due to exists for backward compatibility reasons
+
+  // @HostBinding('class') className = 'control'; 
+
+  //  @HostListener('click') onClick(){
+  //   console.log("Clicked");
+  //  }
+  label = input<string>();
+
+  onClick(){
+    console.log("Clicked");
+  }
 }
