f90a68010eb96034d1333f0408692800b90871f3
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-14 18:05:49 +0700

    create new component for demo of lifecycle component

---
+++ Angular/deep-dive-1-component-template/src/app/life-cycle-component/life-cycle-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-life-cycle-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './life-cycle-component.component.html',
+  styleUrl: './life-cycle-component.component.css'
+})
+export class LifeCycleComponentComponent {
+
+}
