f51108ed54752a34cc95c2b0ef32d62290d7b4a1
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-15 15:01:57 +0700

    131. Getting Access to Template Elements via @ViewChild Decorator of angular.core

--- Angular/deep-dive-1-component-template/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ Angular/deep-dive-1-component-template/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { ButtonComponent } from "../../../shared/button/button.component";
@@ -12,10 +12,14 @@ import { ControlComponent } from "../../../shared/control/control.component";
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  @ViewChild('form') form? : ElementRef<HTMLFormElement>;
 
   onSubmit(title : string, request : string) {
     console.dir(title);
     console.dir(request);
     console.log('SUBMITTED');
+
+    this.form?.nativeElement.reset();
   }
 }
+ 
