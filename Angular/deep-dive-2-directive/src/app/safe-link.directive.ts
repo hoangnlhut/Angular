f99dd10e3c3cc4704553055de71218d48668bf66
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-20 14:02:28 +0700

    DEEP_DIVE DIRECTIVE: 155 Using Attribute Directives To Change Element Behavior using @HostListener / host

--- Angular/deep-dive-2-directive/src/app/safe-link.directive.ts
+++ Angular/deep-dive-2-directive/src/app/safe-link.directive.ts
@@ -1,11 +1,42 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef, Host, HostListener, inject } from "@angular/core";
 
 @Directive({
     selector: 'a[appSafeLink]',
     standalone: true,
+    host:{
+        '(click)': 'onConfirmLeavePage($event)'
+    }
 })
 export class SafeLinkDirective {
+    private el = inject(ElementRef);
+
     constructor() {
-        console.log('SafeLinkDirective instantiated');
+        // this.el.nativeElement.style.backgroundColor = 'brown';
+        // console.log('SafeLinkDirective instantiated');
     }
+
+    private highlight(color: string) {
+        this.el.nativeElement.style.backgroundColor = color;
+    }
+
+    onMouseEnter() {
+        this.highlight('brown');
+    }
+    
+    onMouseLeave() {
+        this.highlight('');
+    }
+
+    onConfirmLeavePage(event: MouseEvent) {
+       const wantsToLeave =  window.confirm('Are you sure you want to leave this page?');
+
+       if (!wantsToLeave) {
+        event.preventDefault();
+       }
+       else{
+        return;
+       }
+    }
+
+ 
 }
