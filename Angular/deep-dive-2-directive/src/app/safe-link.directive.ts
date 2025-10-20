68b1289da3638ca2fd830f1290ef8f83807b8ff7
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-20 14:20:27 +0700

    DEEP_DIVE DIRECTIVE: 156 Working with Inputs in Custom Directives

--- Angular/deep-dive-2-directive/src/app/safe-link.directive.ts
+++ Angular/deep-dive-2-directive/src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Host, HostListener, inject } from "@angular/core";
+import { Directive, ElementRef, Host, HostListener, inject, input } from "@angular/core";
 
 @Directive({
     selector: 'a[appSafeLink]',
@@ -9,23 +9,24 @@ import { Directive, ElementRef, Host, HostListener, inject } from "@angular/core
 })
 export class SafeLinkDirective {
     private el = inject(ElementRef);
+    queryParameter = input('myapp',{alias: 'appSafeLink'});
 
     constructor() {
         // this.el.nativeElement.style.backgroundColor = 'brown';
         // console.log('SafeLinkDirective instantiated');
     }
 
-    private highlight(color: string) {
-        this.el.nativeElement.style.backgroundColor = color;
-    }
+    // private highlight(color: string) {
+    //     this.el.nativeElement.style.backgroundColor = color;
+    // }
 
-    onMouseEnter() {
-        this.highlight('brown');
-    }
+    // onMouseEnter() {
+    //     this.highlight('brown');
+    // }
     
-    onMouseLeave() {
-        this.highlight('');
-    }
+    // onMouseLeave() {
+    //     this.highlight('');
+    // }
 
     onConfirmLeavePage(event: MouseEvent) {
        const wantsToLeave =  window.confirm('Are you sure you want to leave this page?');
@@ -34,6 +35,8 @@ export class SafeLinkDirective {
         event.preventDefault();
        }
        else{
+        const address = (event.target as HTMLAnchorElement).href;
+        (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParameter();
         return;
        }
     }
