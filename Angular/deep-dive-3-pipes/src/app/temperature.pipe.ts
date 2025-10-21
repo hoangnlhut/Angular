022a85f55f5f70eb13b1f6e870fcf0e6109f4d7a
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-21 16:54:35 +0700

    DEEP DIVE PIPES: 168 Accepting Parameters in Custom Pipes

--- Angular/deep-dive-3-pipes/src/app/temperature.pipe.ts
+++ Angular/deep-dive-3-pipes/src/app/temperature.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TemperaturePipe implements PipeTransform {
 
-  transform(value: string | number) {
+  transform(value: string | number, inputType: 'cel' | 'fah', outputType?: 'cel' | 'fah'): string {
     let val: number;
 
     if(typeof value === 'string')
@@ -17,9 +17,28 @@ export class TemperaturePipe implements PipeTransform {
       val = value;
     }
 
+    let outputTemp: number;
+    if(inputType === 'cel' && outputType === 'fah'){
+      outputTemp = val * 9/5 + 32;
+    }
+    else if(inputType === 'fah' && outputType === 'cel'){
+      outputTemp = (val - 32) * (5/9);
+    }
+    else{
+      outputTemp = val;
+    }
+
+    let symbol: '°C' | '°F';
+    if(!outputType){
+      symbol = inputType === 'cel' ? '°C' : '°F';
+    }
+    else{
+      symbol = outputType === 'cel' ? '°C' : '°F';
+    }
+     
+
     //convert Celsius to Fahrenheit
-    const outputTemp = val * 9/5 + 32;
-    return `${outputTemp.toFixed(3)} °F`;
+    return `${outputTemp} ${symbol}`;
   }
 
 }
