58bd7bbed40e7ca8422ad35d120e0e089cf37f3f
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-24 11:19:35 +0700

    DEEP_DIVE_CHANGE_DETECTION : 201. Using Signals for Sharing Data Across Components (with OnPush)

--- Angular/deep-dive-5-change-detection/src/app/messages/messages.component.ts
+++ Angular/deep-dive-5-change-detection/src/app/messages/messages.component.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 
 import { MessagesListComponent } from './messages-list/messages-list.component';
 import { NewMessageComponent } from './new-message/new-message.component';
+import { MessagesService } from './messages.service';
 
 @Component({
   selector: 'app-messages',
@@ -12,14 +13,8 @@ import { NewMessageComponent } from './new-message/new-message.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MessagesComponent {
-  messages = signal<string[]>([]);
-
   get debugOutput() {
     console.log('[Messages] "debugOutput" binding re-evaluated.');
     return 'Messages Component Debug Output';
-  }
-
-  onAddMessage(message: string) {
-    this.messages.update((oldMessages) => [...oldMessages, message]);
   }
 }
