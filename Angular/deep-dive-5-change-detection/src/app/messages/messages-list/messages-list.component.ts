2136dfa268f74007e70b4e1367c8dec9861cc54e
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-24 15:54:54 +0700

    DEEP_DIVE_CHANGE_DETECTION : 204 The async Pipe ( a shortcut for repacting subscribe message RxJS)

--- Angular/deep-dive-5-change-detection/src/app/messages/messages-list/messages-list.component.ts
+++ Angular/deep-dive-5-change-detection/src/app/messages/messages-list/messages-list.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+
 import { MessagesService } from '../messages.service';
 
 @Component({
@@ -6,24 +8,12 @@ import { MessagesService } from '../messages.service';
   standalone: true,
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
+  imports: [AsyncPipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MessagesListComponent implements OnInit {
+export class MessagesListComponent {
   private messagesService = inject(MessagesService);
-  private changeDetectorRef = inject(ChangeDetectorRef);
-  private destroyRef = inject(DestroyRef);
-
-  messages: string[] = [];
-
-  ngOnInit() {
-    const subscription = this.messagesService.messages$.subscribe((messages) => {
-      this.messages = messages;
-      this.changeDetectorRef.markForCheck();
-    })
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
-    });
-  }
+  messages$ = this.messagesService.messages$;
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
