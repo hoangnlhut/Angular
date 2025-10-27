63eca9912325e8cb5fb2c2c4db329ccba03d8375
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-27 14:49:50 +0700

    DEEP_DIVE_RxJS: 210 Working with Signal and effect ( do log when singal change value)

--- Angular/deep-dive-6-RxJS-Observables/src/app/app.component.ts
+++ Angular/deep-dive-6-RxJS-Observables/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
-import { interval, map, take } from 'rxjs';
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import { interval, map, single, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +8,28 @@ import { interval, map, take } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
+  clickCount = signal(0);
+
+  constructor() {
+    effect(() => {
+      console.log(`Button clicked ${this.clickCount()} times`);
+    }); 
+  }
 
   ngOnInit(): void {
-    const subscription = interval(1000).pipe(
-      map(value => value * 2),
-    ).subscribe({
-      next: (value) => console.log(value)
-    });
+    // const subscription = interval(1000).pipe(
+    //   map(value => value * 2),
+    // ).subscribe({
+    //   next: (value) => console.log(value)
+    // });
 
     // this.destroyRef.onDestroy(() => {
     //   console.log('Component is being destroyed, unsubscribing from interval');
     //   subscription.unsubscribe();
     // });
+  }
+
+  onClick(){
+      this.clickCount.update(preCount => preCount + 1);
   }
 }
