088432dda3d753801dc74893903bbaa1023c5606
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-27 16:52:35 +0700

    DEEP_DIVE_RxJS: 213. Converting Observables To Signals

--- Angular/deep-dive-6-RxJS-Observables/src/app/app.component.ts
+++ Angular/deep-dive-6-RxJS-Observables/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, computed, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
-import { toObservable } from '@angular/core/rxjs-interop';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { interval, map, single, take } from 'rxjs';
 
 @Component({
@@ -11,6 +11,11 @@ export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
   clickCount = signal(0);
   clickCount$ = toObservable(this.clickCount);
+
+  inteval$ = interval(1000);
+  //after that you can use computed() or effect() with this signal
+  // and/or ouptut it in the template
+  intevalSignal = toSignal(this.inteval$, { initialValue: 10 });
   // as map in interval
   // changeValue = computed(() => this.clickCount() * 2); 
 
@@ -26,15 +31,15 @@ export class AppComponent implements OnInit {
     // ).subscribe({
     //   next: (value) => console.log(value)
     // });
-
-    // this.destroyRef.onDestroy(() => {
-    //   console.log('Component is being destroyed, unsubscribing from interval');
-    //   subscription.unsubscribe();
-    // });
-    this.clickCount$.pipe(
+    const subscription = this.clickCount$.pipe(
       map(value => value * 2)
     ).subscribe({
       next: (value) => console.log(`Button clicked doubled value: ${value}`)
+    });
+
+    this.destroyRef.onDestroy(() => {
+      console.log('Component is being destroyed, unsubscribing from interval');
+      subscription.unsubscribe();
     });
   }
 onClick(){
