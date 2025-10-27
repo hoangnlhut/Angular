a20ee35c783a37084595e8f949bfdbe355a890f4
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-27 14:13:05 +0700

    DEEP_DIVE_RxJS: 208   Creating & Using an Observable

--- Angular/deep-dive-6-RxJS-Observables/src/app/app.component.ts
+++ Angular/deep-dive-6-RxJS-Observables/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { interval, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
 
+  ngOnInit(): void {
+    const subscription = interval(1000).subscribe({
+      next: (value) => console.log(value),// only care next, could also pass just a function to subscribe().
+      // that would be executed for every emiited value
+      complete: () => console.log('Completed'),
+      error: (err) => console.error('Error: ', err)
+    });
+
+    this.destroyRef.onDestroy(() => {
+      console.log('Component is being destroyed, unsubscribing from interval');
+      subscription.unsubscribe();
+    });
+  }
 }
