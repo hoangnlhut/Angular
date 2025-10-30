8616e2434683612977bf62d28c9ba8c5b5d10e77
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-10-30 14:40:55 +0700

    DEEP_DIVE_Backend HTTP Request and Response 224. Transforming & Using Response Data ( using Pipe() with build-in operator to transform data before subscribe()) 225. Showing a Loading Fallback: define a signal variable to check if the data is fetched 226. Handling HTTP Errors: 1. use operator to define error with catchError() operator and then add error() in subscribe()

--- Angular/deep-dive-7-https-request-response/src/app/places/available-places/available-places.component.ts
+++ Angular/deep-dive-7-https-request-response/src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -16,18 +17,30 @@ export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
+  isFetching = signal(false);
+  error = signal('');
 
   ngOnInit(){
-    const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places', 
-      // {
-      // observe: 'response'
-      // observe: 'events'
-    // }
-  )
+    this.isFetching.set(true);
+    const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places')
+    .pipe(
+      map((resData) => {
+        return resData.places;
+      }),
+      catchError((error) => {
+        //save in log to process for engineer
+        console.log(error);
+       return throwError(() => new Error("An error occurred while fetching places. Please try again later."));
+      }))
     .subscribe({
-      next: (resData) =>{
-        // console.log(resData);
-        console.log(resData.places);
+      next: (data) =>{
+        this.places.set(data);
+      },
+      error: (error : Error) =>{
+        this.error.set(error.message);
+      },
+      complete: () =>{
+        this.isFetching.set(false);
       }
     }
     );
