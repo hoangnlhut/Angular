abe7672c99e391ce3fb6cb33fc0ba73850be3898
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-11-03 17:00:50 +0700

    DEEP_DIVE_Backend HTTP Request and Response 236: HTTP Interceptors

--- Angular/deep-dive-7-https-request-response/src/main.ts
+++ Angular/deep-dive-7-https-request-response/src/main.ts
@@ -1,9 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 
+
+function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn){
+    console.log('Outgoing request:', request);
+    return next(request);
+}
 
 bootstrapApplication(AppComponent, {
-    providers: [provideHttpClient()],
+    providers: [provideHttpClient(  
+        withInterceptors([loggingInterceptor])
+    )],
 }).catch((err) => console.error(err));
