d7699a9f751cdaeccf2df18629fd944eb80f732b
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-11-07 19:06:10 +0700

    DEEP_DIVE_ROUTING: 275. Extracting Dynamic Route Parameters  ( back to new way) and  Working with Nested Routes

--- Angular/deep-dive-9-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ Angular/deep-dive-9-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,38 +1,16 @@
 import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
 import { UsersService } from '../users.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
+  imports: [RouterOutlet],
 })
-export class UserTasksComponent implements OnInit{
-  //new way
-  // userId = input.required<string>();
-  // private userService = inject(UsersService);
-  // userName = computed(() => this.userService.users.find(user => user.id === this.userId())?.name)
-
-  //old way via observable using activatedRoute
-  userName = '';
+export class UserTasksComponent {
+  userId = input.required<string>();
   private userService = inject(UsersService);
-  private activatedRoute = inject(ActivatedRoute);
-  private destroyRef = inject(DestroyRef);
-
-  ngOnInit(): void {
-    console.log(this.activatedRoute);
-    const subscription = this.activatedRoute.paramMap.subscribe({
-      next: (paramMap) => {
-          console.log(paramMap);
-          this.userName = this.userService.users.find(ur => ur.id === paramMap.get('userId'))?.name ?? '';
-      }
-    });
-
-    this.destroyRef.onDestroy(() => 
-    {
-      console.log('destroy ref');
-      subscription.unsubscribe();
-    });
-  }
+  userName = computed(() => this.userService.users.find(user => user.id === this.userId())?.name)
 }
