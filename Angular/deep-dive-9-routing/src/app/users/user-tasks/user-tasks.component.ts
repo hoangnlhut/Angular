7d958562a647527dd7cb44a89556de78044a2da7
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-11-07 14:31:21 +0700

    DEEP_DIVE_ROUTING: 272. Extracting Dynamic Route Parameters via Inputs

--- Angular/deep-dive-9-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ Angular/deep-dive-9-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-user-tasks',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './user-tasks.component.css',
 })
 export class UserTasksComponent {
+  userId = input.required<string>();
+  private userService = inject(UsersService);
+
+  userName = computed(() => this.userService.users.find(user => user.id === this.userId())?.name)
   
 }
