1a38213a18d0f269bc3a6f01b5c50af97bff8d9c
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-11-10 11:13:14 +0700

    DEEP_DIVE_ROUTING: 287. Using Query Parameters For Data Manipulation

--- Angular/deep-dive-9-routing/src/app/tasks/tasks.component.ts
+++ Angular/deep-dive-9-routing/src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, DestroyRef, inject, input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, OnChanges, OnInit, signal, SimpleChanges } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
@@ -14,20 +14,28 @@ import { ActivatedRoute, RouterLink } from "@angular/router";
 })
 export class TasksComponent implements OnInit{
   private taskService = inject(TasksService);
+
+  order = signal<'asc' | 'desc'>('desc');
+
   userId = input.required<string>();
-  userTasks = computed(() => this.taskService.allTasks().filter(task => task.userId === this.userId()));
+  userTasks = computed(() => 
+    this.taskService.allTasks()
+    .filter(task => task.userId === this.userId())
+    .sort((a, b) => 
+      this.order() === 'desc' ? (a.id > b.id ? -1 : 1) : (a.id > b.id ? 1 : -1))
+  );
 
   //1. input signal ways
   // order = input<'asc' | 'desc'>();
 
   //2. activatedRoute observable way
-  order? : 'asc' | 'desc';
+  // order? : 'asc' | 'desc';
   private activatedRoute = inject(ActivatedRoute);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
     const subscription = this.activatedRoute.queryParams.subscribe({
-      next: param => this.order = param['order']
+      next: param => this.order.set(param['order'])
     });
 
     this.destroyRef.onDestroy(() => subscription.unsubscribe());
