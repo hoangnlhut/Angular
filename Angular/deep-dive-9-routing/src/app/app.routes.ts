fbe0b0516619eeaba4c97ba9adb574e864cdfadf
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-11-11 11:13:27 +0700

    DEEP_DIVE_ROUTING: 294. Introducing Route Guards and canMatch guard routing

--- Angular/deep-dive-9-routing/src/app/app.routes.ts
+++ Angular/deep-dive-9-routing/src/app/app.routes.ts
@@ -1,8 +1,19 @@
-import { Routes } from "@angular/router";
+import { CanMatchFn, RedirectCommand, Router, Routes } from "@angular/router";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { resolveUsername, resolveUserTaskTitle, UserNameResolverClass, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 import {routes as routesUser} from "./users/users.routes";
+import { inject } from "@angular/core";
+
+export const dummyCanMatch: CanMatchFn = (route, segments) => {
+    const router = inject(Router);
+    const shouldGetAccess = Math.random();
+    if(shouldGetAccess < 0.5) return true;
+
+    return new RedirectCommand(router.parseUrl('/unauthorized'));
+
+};
+
 
 export const routes: Routes = [
     {
@@ -14,6 +25,7 @@ export const routes: Routes = [
         path: 'users/:userId', // <your-domain>/users/<uid>
         component: UserTasksComponent,
         children: routesUser,
+        canMatch: [dummyCanMatch],
         data: {
             message: 'Hello world'
         },
