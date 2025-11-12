758f4e9ff87eb579035fe13d36418ba525127f44
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-11-12 19:06:26 +0700

    DEEP_DIVE_LAZYLOADING: 304 lazy load service final

---
+++ Angular/deep-dive-10-lazy-loading/src/app/users/users.routes.ts
@@ -0,0 +1,33 @@
+import { Routes } from '@angular/router';
+
+//this is eager loading
+import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
+import { resolveUserTasks, TasksComponent } from '../tasks/tasks.component';
+import { TasksService } from '../tasks/tasks.service';
+
+export const routes: Routes = [
+  {
+    path: '',
+    providers: [TasksService], // this service will be used in component/ route of user tasks
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        component: TasksComponent,
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage]
+      },
+    ]
+  }
+];
