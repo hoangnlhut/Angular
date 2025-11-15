1bb7c49a3555c0f207ce38cd71d7b5957d33f1bd
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-11-15 16:18:45 +0700

    DEEP_DIVE_DEPLOYMENT_SSR: initialize new project for SSR deployment

---
+++ Angular/deep-dive-11-deployment/SSR/src/app/app.config.ts
@@ -0,0 +1,21 @@
+import { ApplicationConfig } from '@angular/core';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withRouterConfig,
+} from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withRouterConfig({
+        paramsInheritanceStrategy: 'always',
+      })
+    ), provideClientHydration(withEventReplay()),
+  ],
+};
