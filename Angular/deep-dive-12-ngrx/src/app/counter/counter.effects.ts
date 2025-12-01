2f051f980c75350e616ee7f2ec1e364c41ad8e88
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2025-12-01 11:10:31 +0700

    DEEP_DIVE_NGRX: 700. Using Store Data In Effects ( using withLatestFrom() in pipe)

--- Angular/deep-dive-12-ngrx/src/app/counter/counter.effects.ts
+++ Angular/deep-dive-12-ngrx/src/app/counter/counter.effects.ts
@@ -1,18 +1,21 @@
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { decrement, increment } from "./counter.actions";
-import { tap } from "rxjs";
+import { tap, withLatestFrom } from "rxjs";
 import { Injectable } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { selectCount } from "./counter.selectors";
 
 @Injectable()
 export class CounterEffects{
     saveCount = createEffect(() => this.action$.pipe(
         ofType(increment, decrement),
-        tap(action => {
-            console.log('Saving count to local storage:', action);
+        withLatestFrom(this.store.select(selectCount)),
+        tap(([action, counter]) => {
+            console.log(`Saving count ${action} to local storage : ${counter}`);
             // Here you can add logic to save the count to local storage or make an API call
-            localStorage.setItem('counter', action.value.toString());
+            localStorage.setItem('counter', counter.toString());
         })  
     ), { dispatch: false }); 
 
-    constructor(private action$: Actions) {}
+    constructor(private action$: Actions, private store: Store<{counter: number}>) {}
 }
